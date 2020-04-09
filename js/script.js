3c973563c3f9d2ff0556cd39c6d7934bae1ae041
Kelly Born <kellyaborn@gmail.com>
2020-04-08 22:17:36 -0400

    Final commit for deliverable two

--- js/script.js
+++ js/script.js
@@ -1,42 +1,34 @@
 var vacationType = prompt("What type of trip do you want to go on, musical, tropical, or adventurous?");
 var groupSize = prompt("How many people are you traveling with?");
 
-if(vacationType === "musical" && groupSize <= "2"){
-var result = "First class";
-    console.log("Since you are a group of" + " " + groupSize + " " + "going on a" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to New Orleans" + ".");
+let destination = "";
+let travelSuggestion = "";
+if (vacationType === "musical") {
+  destination = "New Orleans";
+} else if (vacationType === "tropical") {
+  destination = "a beach vacation in Mexico";
+} else if (vacationType === "adventurous") {
+  destination = "whitewater rafting the Grand Canyon";
 }
-else if (vacationType === "musical" && (groupSize >= "3" && groupSize <= "5")){
-    var result = "a Helicopter";
-    console.log("Since you are a group of" + " " + groupSize + " " + "going on a" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to New Orleans" + ".");
+if (groupSize <= "2") {
+  travelSuggestion = "First Class flight";
+} else if (groupSize >= "3" && groupSize <= "5") {
+  travelSuggestion = "Helicopter";
+} else if (groupSize >= "6") {
+  travelSuggestion = "Charter Flight";
 }
-else if (vacationType === "musical" && groupSize >= "6"){
-    var result = "a Charter Flight";
-    console.log("Since you are a group of" + " " + groupSize + " " + "going on a" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to New Orleans" + ".");
-}
+let result =
+  "Since you are a group of " +
+  groupSize +
+  " going on a " +
+  vacationType +
+  " vacation, " +
+  "you should take" + " " + "a" + " " +
+  travelSuggestion +
+  " " +
+  "to " +
+  destination +
+  ".";
 
-if(vacationType === "tropical" && groupSize <= "2"){
-    var result = "First class";
-        console.log("Since you are a group of" + " " + groupSize + " " + "going on a" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to a beach vacation in Mexico" + ".");
-    }
-else if (vacationType === "tropical" && (groupSize >= "3" && groupSize <= "5")){
-        var result = "a Helicopter";
-        console.log("Since you are a group of" + " " + groupSize + " " + "going on a" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to a beach vacation in Mexico" + ".");
-    }
-else if (vacationType === "tropical" && groupSize >= "6"){
-        var result = "a Charter Flight";
-        console.log("Since you are a group of" + " " + groupSize + " " + "going on a" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to a beach vacation in Mexico" + ".");
-}
+console.log(result);
 
-if(vacationType === "adventurous" && groupSize <= "2"){
-    var result = "First class";
-        console.log("Since you are a group of" + " " + groupSize + " " + "going on an" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to a beach vacation in Mexico" + ".");
-    }
-else if (vacationType === "adventurous" && (groupSize >= "3" && groupSize <= "5")){
-        var result = "a Helicopter";
-        console.log("Since you are a group of" + " " + groupSize + " " + "going on an" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to a beach vacation in Mexico" + ".");
-    }
-else if (vacationType === "adventurous" && groupSize >= "6"){
-        var result = "a Charter Flight";
-        console.log("Since you are a group of" + " " + groupSize + " " + "going on an" + " " + vacationType + " " + "vacation," + " " + "you should take" + " " + result + " " + "to a beach vacation in Mexico" + ".");
-}
- 
